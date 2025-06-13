671cf83d37efee1800216971a04cdeb5ffb3b5f9
SABB!R <138793371+sabbirexist@users.noreply.github.com>
2025-06-14 02:46:03 +0600

    Update streams.js

--- config/streams.js
+++ config/streams.js
@@ -8,8 +8,8 @@ const STREAMS = {
     url: "https://smart.bengaldigital.live/star-sports-1/index.m3u8",
     headers: {
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
-      'Referer': 'https://smart.bengaldigital.live/',
-      'Origin': 'https://smart.bengaldigital.live',
+      'Referer': 'https://stxproxy.onrender.com',
+      'Origin': 'https://stxproxy.onrender.com',
       'Accept': '*/*',
       'Accept-Language': 'en-US,en;q=0.9',
       'Accept-Encoding': 'identity'
