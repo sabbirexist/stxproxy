51584fcf6e34b4fbea6f5a2af235d76b964676ca
SABB!R <138793371+sabbirexist@users.noreply.github.com>
2025-06-14 03:00:18 +0600

    Update streams.js

--- config/streams.js
+++ config/streams.js
@@ -2,7 +2,6 @@ const STREAMS = {
   star_sports_1: {
     url: "https://smart.bengaldigital.live/star-sports-1/index.m3u8",
     headers: {
-      'Host': 'smart.bengaldigital.live',
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:139.0) Gecko/20100101 Firefox/139.0',
       'Accept': '*/*',
       'Accept-Language': 'en-US,en;q=0.5',
