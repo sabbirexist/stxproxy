9df99376cf31e6f04dfd7bf90fe897172466f210
SABB!R <138793371+sabbirexist@users.noreply.github.com>
2025-06-14 02:54:59 +0600

    Update streams.js

--- config/streams.js
+++ config/streams.js
@@ -1,18 +1,18 @@
-/**
- * STREAMS configuration
- * Add your streams here with unique keys, URLs, and optional headers.
- */
-
 const STREAMS = {
   star_sports_1: {
     url: "https://smart.bengaldigital.live/star-sports-1/index.m3u8",
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
-      'Referer': 'https://stxproxy.onrender.com',
-      'Origin': 'https://stxproxy.onrender.com',
+      'Host': 'smart.bengaldigital.live',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:139.0) Gecko/20100101 Firefox/139.0',
       'Accept': '*/*',
-      'Accept-Language': 'en-US,en;q=0.9',
-      'Accept-Encoding': 'identity'
+      'Accept-Language': 'en-US,en;q=0.5',
+      'Accept-Encoding': 'gzip, deflate, br, zstd',
+      'Origin': 'https://anym3u8player.com',
+      'Connection': 'keep-alive',
+      'Sec-Fetch-Dest': 'empty',
+      'Sec-Fetch-Mode': 'cors',
+      'Sec-Fetch-Site': 'cross-site',
+      'TE': 'trailers'
     }
   },
 
